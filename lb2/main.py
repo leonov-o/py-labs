3b812d86ebc368a9587da9cccf3dcdacd2ea8596
Oleksandr Leonov <oleksandr.leonov.work@gmail.com>
2024-04-01 13:02:20 +0300

    lb2

---
+++ lb2/main.py
@@ -0,0 +1,5 @@
+import geometry_operations
+
+print(geometry_operations.calculate_circle_area(12))
+print(geometry_operations.calculate_rectangle_area(12, 6))
+print(geometry_operations.calculate_triangle_area(9, 6))
